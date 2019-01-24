9ac122a2aab113408ecc99c64d23c4e36fd06905
Will Eggleston <work.willeggleston@gmail.com>
2019-01-24 07:59:38 -0600

    arduino calc

--- arduino/linefollower/linefollower.ino
+++ arduino/linefollower/linefollower.ino
@@ -1,26 +1,34 @@
 #include <QTRSensors.h>
+#include <math.h>
+#define PI 3.14159265
+
+QTRSensorsRC sensor1((unsigned char[]) {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37}, 16);
+QTRSensorsRC sensor2((unsigned char[]) {38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53}, 16);
+unsigned int sensor1_values[16];
+unsigned int sensor2_values[16];
+unsigned int line1;
+unsigned int line2;
+unsigned double angle;
+unsigned double dist;
 
 void setup() {
-  QTRSensorsRC sensor1((unsigned char[]) {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37}, 16);
-  QTRSensorsRC sensor2((unsigned char[]) {38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53}, 16);
-  unsigned int sensor1_values[16];
-  unsigned int sensor2_values[16];
-  Serial.begin(9600);
+  //Serial.begin(9600);
 }
 
 void loop()
 {
-  sensor1.read(sensor1_values);
-  sensor2.read(sensor2_values);
-  for (int i = 0; i <16; i++){
-    Serial.print(sensor1_values[i]);
-    Serial.print('\t');
-  }
-  Serial.print('\n');  
-  for (int i = 0; i <16; i++){
-    Serial.print(sensor2_values[i]);
-    Serial.print('\t');
-  }
-  Serial.print('\n');
-  delay(1000);
+  sensor1.read(sensor1_values, readMode=QTR_EMITTERS_OFF);
+  line1 = sensor1.readLine(sensor1_values, readMode=QTR_EMITTERS_OFF, whiteLine=1)
+  sensor2.read(sensor1_values, readMode=QTR_EMITTERS_OFF);
+  line2 = sensor1.readLine(sensor1_values, readMode=QTR_EMITTERS_OFF, whiteLine=1)
+  angle = atan((line1-line2)/1600.0);
+  dist = (8500-(line1+line2))/2000.0;
+  /*
+  Serial.print(line1); 
+  Serial.print('\t'); 
+  Serial.print(line2);
+  Serial.print('\n'); 
+  delay(200); 
+  */
+
 }
